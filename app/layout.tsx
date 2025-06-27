c454fc47d2deed13e143b3e952febd683f393ab3
jKALI10 <jerymlop@gmail.com>
2025-06-27 09:42:33 +0300

    update12

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import type React from "react"
-import "@/app/globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import type React from "react";
+import "@/app/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Design Studio | Professional Graphic Design Portfolio",
+  title: "Motiv Studio",
   description:
-    "Award-winning graphic designer creating bold, innovative designs that elevate brands and captivate audiences.",
-    generator: 'v0.dev'
-}
+    "Creating bold, innovative designs that elevate brands and captivate audiences",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
